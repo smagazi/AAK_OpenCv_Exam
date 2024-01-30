7e821cc82a0e7c87ba34214f47ea5c02d2d1f2c4
Sara Magaziotis-Ginori <smagazi@berkeley.edu>
2024-01-30 02:08:16 -0800

    first commit, initializing repo, file has relevant imports

---
+++ Object_Detection_w_Voice_Commands.py
@@ -0,0 +1,10 @@
+import cv2
+import cvlib as cv2
+from cvlib.object_detection import draw_bbox
+from gtts import gTTS
+from playsound import playsound
+
+video_stream = cv2.VideoCapture(1)
+
+while True:
+    continue
